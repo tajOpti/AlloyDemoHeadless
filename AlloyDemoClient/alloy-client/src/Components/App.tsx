9a44ca45b489d242f6b608d69aa42dd9e106e98a
tajOpti <tajbiul.rawol@optimizely.com>
2024-04-04 01:36:21 +0600

    React Client app added with axios to fetch data from CD api, might not need node server

--- AlloyDemoClient/alloy-client/src/Components/App.tsx
+++ AlloyDemoClient/alloy-client/src/Components/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
-}
+const App = () => (
+  <h1>hello</h1>
+);
 
 export default App;
